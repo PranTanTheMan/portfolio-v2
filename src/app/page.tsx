f33a9197ae228b4ad8b866627c12b66ceeeb34e9
Pranith Molakalapalli <46548127+PranTanTheMan@users.noreply.github.com>
2024-02-16 18:36:21 -0700

    Refactor page component layout and add dot patterns

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,42 @@
-import Image from "next/image";
+import { cn } from "@/lib/utils";
+import DotPattern from "@/components/dot-pattern";
 
 export default function Home() {
   return (
-    <div className="">
-      <div className="grid grid-cols-4 gap-4 p-4">
-        <div className="col-span-1 bg-white h-[327px] rounded-xl backdrop-filter backdrop-blur-lg bg-opacity-15"></div>
-        <div className="col-span-1 bg-white h-[327px] rounded-xl backdrop-filter backdrop-blur-lg bg-opacity-15"></div>
-        <div className="col-span-2 bg-white h-auto rounded-xl backdrop-filter backdrop-blur-lg bg-opacity-15"></div>
-
-        <div className="col-span-3 bg-white h-[550px] rounded-xl backdrop-filter backdrop-blur-lg bg-opacity-15"></div>
-        <div className="col-span-1 bg-white h-[550px] rounded-xl backdrop-filter backdrop-blur-lg bg-opacity-15"></div>
+    <>
+      <div className="relative flex h-full w-full items-center justify-center overflow-hidden rounded-lg bg-background p-[20rem] md:shadow-2xl">
+        <div className="z-10 whitespace-pre-wrap text-9xl tracking-tight text-white flex flex-col">
+          <span>Pranith</span>
+          <span>Molakalapalli</span>
+          <div
+            className={`font-gopher text-5xl flex w-full justify-between pt-10 tracking-wide`}
+          >
+            <span>18</span>
+            <span>Full-Stack</span>
+            <span>Arizona</span>
+          </div>
+        </div>
+        <DotPattern
+          width={20}
+          height={20}
+          cx={1}
+          cy={1}
+          cr={1}
+          className={cn(
+            "[mask-image:linear-gradient(to_bottom_right,white,transparent,transparent)] "
+          )}
+        />
+        <DotPattern
+          width={20}
+          height={20}
+          cx={1}
+          cy={1}
+          cr={1}
+          className={cn(
+            "[mask-image:linear-gradient(to_top_left,white,transparent,transparent)] "
+          )}
+        />
       </div>
-    </div>
+    </>
   );
 }
