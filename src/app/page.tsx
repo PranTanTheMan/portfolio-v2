6cd45dbf7749640b43de5a78b882c6d74863233d
PranTanTheMan <Seriousbro23@gmail.com>
2024-02-16 23:48:31 -0700

    heheha

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,16 @@ import DotPattern from "@/components/dot-pattern";
 export default function Home() {
   return (
     <>
-      <div className="relative flex h-full w-full items-center justify-center overflow-hidden rounded-lg bg-background p-[20rem] md:shadow-2xl">
-        <div className="z-10 whitespace-pre-wrap text-9xl tracking-tight text-white flex flex-col">
+      <div className="bg-background relative flex h-full w-full items-center justify-center overflow-hidden rounded-lg p-[20rem] md:shadow-2xl">
+        <div className="z-10 flex flex-col whitespace-pre-wrap text-9xl tracking-tight text-white">
           <span>Pranith</span>
           <span>Molakalapalli</span>
           <div
-            className={`font-gopher text-5xl flex w-full justify-between pt-10 tracking-wide`}
+            className={`font-gopher flex w-full justify-between pt-10 text-5xl tracking-wide`}
           >
-            <span>18</span>
+            {/* <span>18</span>
             <span>Full-Stack</span>
-            <span>Arizona</span>
+            <span>Arizona</span> */}
           </div>
         </div>
         <DotPattern
@@ -23,7 +23,7 @@ export default function Home() {
           cy={1}
           cr={1}
           className={cn(
-            "[mask-image:linear-gradient(to_bottom_right,white,transparent,transparent)] "
+            "[mask-image:linear-gradient(to_bottom_right,white,transparent,transparent)] ",
           )}
         />
         <DotPattern
@@ -33,7 +33,7 @@ export default function Home() {
           cy={1}
           cr={1}
           className={cn(
-            "[mask-image:linear-gradient(to_top_left,white,transparent,transparent)] "
+            "[mask-image:linear-gradient(to_top_left,white,transparent,transparent)] ",
           )}
         />
       </div>
