5dbde99bf5264ce9f729cfaed47b868eaf9dd371
Pranith Molakalapalli <46548127+PranTanTheMan@users.noreply.github.com>
2024-02-20 18:20:33 -0700

    Refactor page component and add animated cursor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,26 @@
 "use client";
-
-import { cn } from "@/lib/utils";
-import DotPattern from "@/components/dot-pattern";
-import details from "@/utils/details.json";
+import Hero from "@/components/hero";
+import About from "@/components/about";
+import AnimatedCursor from "react-animated-cursor";
 
 export default function Home() {
   return (
     <>
-      <div className="bg-background relative flex h-full w-full items-center justify-center overflow-hidden rounded-lg p-[20rem] md:shadow-2xl">
-        <div className="z-10 flex flex-col whitespace-pre-wrap text-9xl tracking-tight text-white">
-          <span>Pranith</span>
-          <span>Molakalapalli</span>
-          <div
-            className={`font-gopher flex w-full justify-between pt-4 text-5xl tracking-wide`}
-          >
-            {details.detail.map((item: String, i: any) => (
-              <span className="font-thin italic" key={i}>
-                {item}
-              </span>
-            ))}
-          </div>
-        </div>
-        <DotPattern
-          width={20}
-          height={20}
-          cx={2}
-          cy={2}
-          cr={1}
-          className={cn(
-            "[mask-image:linear-gradient(to_bottom_right,white,transparent,transparent)] "
-          )}
-        />
-        <DotPattern
-          width={20}
-          height={20}
-          cx={2}
-          cy={2}
-          cr={1}
-          className={cn(
-            "[mask-image:linear-gradient(to_top_left,white,transparent,transparent)] "
-          )}
-        />
-      </div>
+      <AnimatedCursor
+        innerSize={7}
+        outerSize={40}
+        innerScale={1}
+        outerScale={3}
+        outerAlpha={0}
+        innerStyle={{
+          backgroundColor: "var(--cursor-color)",
+        }}
+        outerStyle={{
+          border: "2px solid var(--cursor-color-outer)",
+        }}
+      />
+      <Hero />
+      <About />
     </>
   );
 }
