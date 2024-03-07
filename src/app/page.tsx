ebb39bd317469fb5fc4b581965720774b0f1ca16
Pranith Molakalapalli <46548127+PranTanTheMan@users.noreply.github.com>
2024-03-07 00:06:10 -0600

    Refactor page component to include new components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,20 @@
 "use client";
 import Hero from "@/components/hero";
-import About from "@/components/about";
-import AnimatedCursor from "react-animated-cursor";
+import Projects from "@/components/projects";
+import Experience from "@/components/experience";
+import Contact from "@/components/contact";
+import AboutV2 from "@/components/about-two";
 
 export default function Home() {
   return (
     <>
-      <AnimatedCursor
-        innerSize={7}
-        outerSize={40}
-        innerScale={1}
-        outerScale={3}
-        outerAlpha={0}
-        innerStyle={{
-          backgroundColor: "var(--cursor-color)",
-        }}
-        outerStyle={{
-          border: "2px solid var(--cursor-color-outer)",
-        }}
-      />
       <Hero />
-      <About />
+      <div id="about" className="h-[5rem] w-full"></div>
+      <AboutV2 />
+      <Projects />
+      <Experience />
+      <Contact />
+      <div className="h-24 w-full md:h-0"></div>
     </>
   );
 }
